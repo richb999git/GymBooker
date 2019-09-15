06e7b801d364cb2cc3e28ed016294982c29030e0
richb999git <richb999@tiscali.co.uk>
2019-09-15 22:43:37 +0100

    Commented file

--- GymBooker1/App_Start/Startup.Auth.cs
+++ GymBooker1/App_Start/Startup.Auth.cs
@@ -58,11 +58,11 @@ namespace GymBooker1
             //   appId: "",
             //   appSecret: "");
 
-            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            {
-                ClientId = "",
-                ClientSecret = ""
-            });
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
         }
     }
 }
